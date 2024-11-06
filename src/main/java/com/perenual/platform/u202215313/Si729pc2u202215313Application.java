4320653f077f88c7784d8fe169870680e93c6e08
rivacortez <rivaldo062134@gmail.com>
2024-11-05 22:16:39 -0500

    chore: initial project

---
+++ src/main/java/com/perenual/platform/u202215313/Si729pc2u202215313Application.java
@@ -0,0 +1,13 @@
+package com.perenual.platform.u202215313;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729pc2u202215313Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729pc2u202215313Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/perenual/platform/u202215313/Si729pc2u202215313ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perenual.platform.u202215313;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729pc2u202215313ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
